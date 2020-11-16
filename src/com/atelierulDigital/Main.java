91cf8a4535f46747cca9ccb348abc61de741b3f9
egdorin <egdorin@gmail.com>
2020-11-16 21:23:24 +0200

    challenge1

--- src/com/atelierulDigital/Main.java
+++ src/com/atelierulDigital/Main.java
@@ -3,6 +3,18 @@ package com.atelierulDigital;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        for(int i =1; i<=100;i++){
+            if(i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FIZZBUZZ");
+            }else if(i % 5 == 0){
+                System.out.println("BUZZ");
+            } else if(i % 3 == 0 ) {
+                System.out.println("Fizz");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
     }
 }
