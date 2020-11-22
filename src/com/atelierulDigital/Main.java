473066a80362ec83fad4b02d367c6eff3f3db18e
egdorin <egdorin@gmail.com>
2020-11-22 16:10:21 +0200

    firstChallenge

--- src/com/atelierulDigital/Main.java
+++ src/com/atelierulDigital/Main.java
@@ -3,12 +3,21 @@ package com.atelierulDigital;
 public class Main {
 
     public static void main(String[] args) {
+        firstChallenge();
 
+    }
+
+    public static void firstChallenge() {
         for(int i =1; i<=100;i++){
+
             if(i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FIZZBUZZ");
             }else if(i % 5 == 0){
                 System.out.println("BUZZ");
+            } else if(i % 11 == 0 ){
+                System.out.println("JAZZ");
+            } else if(i % 7 == 0 ){
+                System.out.println("Rizz");
             } else if(i % 3 == 0 ) {
                 System.out.println("Fizz");
             }
@@ -17,4 +26,6 @@ public class Main {
             }
         }
     }
+
+
 }
